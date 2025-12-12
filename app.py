f740f23425c639234bc5f1514b1859792c843db5
dzakyw <96814015+dzakyw@users.noreply.github.com>
2025-12-12 15:58:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ def load_dem(filelike):
 st.markdown(
     f"""
     <div style="display:flex; align-items:center;">
-        <img src="{https://raw.githubusercontent.com/dzakyw/AutoGrav/main/logo esdm.png}" style="width:200px; margin-right:5px;">
+        <img src="https://raw.githubusercontent.com/dzakyw/AutoGrav/main/logo esdm.png" style="width:200px; margin-right:5px;">
         <div>
             <h2 style="margin-bottom:0;">Auto Grav - Semua Terasa Cepat</h2>
         </div>
@@ -461,6 +461,7 @@ if run:
 
     # download
     st.download_button("Download CSV", df_all.to_csv(index=False).encode("utf-8"), "gravcore_output.csv")
+
 
 
 
