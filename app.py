090d5be80a86c9e6b22d5579a83cc21886133e85
dzakyw <96814015+dzakyw@users.noreply.github.com>
2025-12-12 15:17:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -222,6 +222,19 @@ def load_dem(filelike):
 # -----------------------
 # UI
 # -----------------------
+LOGO_ESDM = "https://raw.githubusercontent.com/dzakyw/AutoGrav/main/logo.png"
+
+# Header kiri
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.image(LOGO_ESDM, width=80)
+with col2:
+    st.markdown("""
+        <h2 style='margin-bottom:0;'>Auto Gravity Processing</h2>
+        <p style='margin-top:0;'>Modul Pengolahan Gravity</p>
+    """, unsafe_allow_html=True)
+
+st.markdown("---")  # garis pemisah
 # ===========================
 # SIMPLE LOGIN MODULE
 # ===========================
@@ -412,6 +425,7 @@ if run:
 
     # download
     st.download_button("Download CSV", df_all.to_csv(index=False).encode("utf-8"), "gravcore_output.csv")
+
 
 
 
